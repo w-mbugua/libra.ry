5803e0a54828861a2daa7007365b398772de7ab9
JoyMbugua <mbuguajoy52@gmail.com>
2023-03-21 07:13:51 +0300

    feat(redis): client setup

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,11 @@ import { AuthorResolver } from './resolvers/Author';
 import { MyContext } from './types';
 import { json } from 'body-parser';
 import { TagResolver } from './resolvers/Tag';
+import connectRedis from 'connect-redis';
+import session from 'express-session';
+import Redis from 'ioredis';
+
+const RedisStore = connectRedis(session);
 
 async function main() {
   const orm = await MikroORM.init(config);
@@ -21,10 +26,36 @@ async function main() {
   const pending = await migrator.getPendingMigrations();
   await migrator.up();
   console.log('PENDING:', pending.length);
-
   await RequestContext.createAsync(orm.em, async () => {});
 
   const app = express();
+
+  const redisClient = new Redis(process.env.REDIS_URL as string);
+  const corsOptions = {
+    credentials: true,
+    origin: process.env.CORS_ORIGIN,
+  };
+
+  const redisStore = new RedisStore({
+    client: redisClient,
+    prefix: 'lib:',
+  });
+
+  app.use(
+    session({
+      name: 'swq',
+      store: redisStore,
+      saveUninitialized: true,
+      secret: process.env.SESSION_SECRET as string,
+      resave: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: false,
+      },
+    })
+  );
 
   // request handlers
   app.get('/ping', (_req, res) => {
